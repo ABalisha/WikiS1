9c3ead73684713f9f942c077f13648f13e708240
Ardit Balisha <arditbalisha01@gmail.com>
2021-11-23 10:33:28 +0100

    First Commit

--- Search.mjs
+++ Search.mjs
@@ -22,7 +22,7 @@ _.map(processes,async val => {
         
         }
     }
-})
+}) //comment 
 _.map(processes,async val => { 
     if(val == 'en'){
         const baseURLsq = "https://en.wikipedia.org/";
